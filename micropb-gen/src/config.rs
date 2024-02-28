c0e2a2e5cbbf92d980caa9dfcdca62acfce904de
YuhanLiin <yuhanliin+github@protonmail.com>
2024-02-27 20:38:42 -0500

    Add tests for config

--- micropb-gen/src/config.rs
+++ micropb-gen/src/config.rs
@@ -37,7 +37,7 @@ impl IntType {
     pub(crate) fn is_signed(self) -> bool {
         matches!(
             self,
-            IntType::I8 | IntType::I16 | IntType::I32 | IntType::Isize
+            IntType::I8 | IntType::I16 | IntType::I32 | IntType::Isize | IntType::I64
         )
     }
 }
@@ -49,7 +49,7 @@ pub enum CustomField {
 }
 
 macro_rules! config_decl {
-    ($($(#[$attr:meta])* $field:ident $([$placeholder:ident])?: Option<$type:ty>,)+) => {
+    ($($(#[$attr:meta])* $([$placeholder:ident])? $field:ident : $([$placeholder2:ident])? Option<$type:ty>,)+) => {
         #[non_exhaustive]
         #[derive(Debug, Clone, Default)]
         pub struct Config {
@@ -62,10 +62,10 @@ macro_rules! config_decl {
             }
 
             pub fn merge(&mut self, other: &Self) {
-                $(config_decl!(@merge $field $([$placeholder])?, self, other);)+
+                $(config_decl!(@merge $([$placeholder])? $field, self, other);)+
             }
 
-            $(config_decl!(@setter $field: $type);)+
+            $(config_decl!(@setter $field: $([$placeholder2])? $type);)+
         }
     };
 
@@ -75,11 +75,11 @@ macro_rules! config_decl {
         }
     };
 
-    (@merge $field:ident [no_inherit], $self:ident, $other:ident) => {
+    (@merge [no_inherit] $field:ident, $self:ident, $other:ident) => {
         $self.$field = $other.$field.clone();
     };
 
-    (@setter $field:ident: String) => {
+    (@setter $field:ident: [deref] $type:ty) => {
         pub fn $field(mut self, s: &str) -> Self {
             self.$field = Some(s.to_owned());
             self
@@ -99,19 +99,19 @@ config_decl! {
     max_len: Option<u32>,
     max_bytes: Option<u32>,
     int_type: Option<IntType>,
-    field_attributes: Option<String>,
+    field_attributes: [deref] Option<String>,
     boxed: Option<bool>,
-    vec_type: Option<String>,
-    string_type: Option<String>,
-    map_type: Option<String>,
+    vec_type: [deref] Option<String>,
+    string_type: [deref] Option<String>,
+    map_type: [deref] Option<String>,
     no_hazzer: Option<bool>,
-    custom_field [no_inherit]: Option<CustomField>,
-    rename_field [no_inherit]: Option<String>,
+    [no_inherit] custom_field: Option<CustomField>,
+    [no_inherit] rename_field: [deref] Option<String>,
 
     // Type configs
     enum_int_type: Option<IntType>,
-    type_attributes: Option<String>,
-    hazzer_attributes: Option<String>,
+    type_attributes: [deref] Option<String>,
+    hazzer_attributes: [deref] Option<String>,
     no_debug_derive: Option<bool>,
 
     // General configs
@@ -187,4 +187,100 @@ pub struct GenConfig {
     pub(crate) use_std: bool,
 
     pub(crate) field_configs: PathTree<Box<Config>>,
+}
+
+#[cfg(test)]
+mod tests {
+    use quote::{format_ident, quote, ToTokens};
+
+    use super::*;
+
+    #[test]
+    fn merge() {
+        let mut mergee = Config::new()
+            .rename_field("rename")
+            .skip(true)
+            .vec_type("vec")
+            .string_type("str");
+        let merger = Config::new().skip(false).vec_type("array");
+        mergee.merge(&merger);
+
+        assert!(!mergee.skip.unwrap());
+        assert_eq!(mergee.vec_type.unwrap(), "array");
+        assert_eq!(mergee.string_type.unwrap(), "str");
+        // max_len was never set
+        assert!(mergee.max_len.is_none());
+        // rename_field gets overwritten unconditionally when merging
+        assert!(mergee.rename_field.is_none());
+    }
+
+    #[test]
+    fn parse() {
+        let mut config = Config::new()
+            .vec_type("heapless::Vec")
+            .string_type("heapless::String")
+            .map_type("Map")
+            .hazzer_attributes("#[derive(Eq)]")
+            .type_attributes("#[derive(Hash)]");
+
+        assert_eq!(
+            config
+                .vec_type_parsed()
+                .unwrap()
+                .to_token_stream()
+                .to_string(),
+            quote! { heapless::Vec }.to_string()
+        );
+        assert_eq!(
+            config
+                .string_type_parsed()
+                .unwrap()
+                .to_token_stream()
+                .to_string(),
+            quote! { heapless::String }.to_string()
+        );
+        assert_eq!(
+            config
+                .map_type_parsed()
+                .unwrap()
+                .to_token_stream()
+                .to_string(),
+            "Map"
+        );
+        assert_eq!(
+            config.hazzer_attr_parsed().to_string(),
+            quote! { #[derive(Eq)] }.to_string()
+        );
+        assert_eq!(
+            config.type_attr_parsed().to_string(),
+            quote! { #[derive(Hash)] }.to_string()
+        );
+
+        assert_eq!(config.field_attr_parsed().to_string(), "");
+        config.field_attributes = Some("#[default]".to_owned());
+        assert_eq!(
+            config.field_attr_parsed().to_string(),
+            quote! { #[default] }.to_string()
+        );
+
+        assert_eq!(config.rust_field_name("name"), format_ident!("name"));
+        config.rename_field = Some("rename".to_string());
+        assert_eq!(config.rust_field_name("name"), format_ident!("rename"));
+
+        config.custom_field = Some(CustomField::Type("Vec<u16, 4>".to_owned()));
+        let crate::generator::CustomField::Type(typ) = config.custom_field_parsed().unwrap() else {
+            unreachable!()
+        };
+        assert_eq!(
+            typ.to_token_stream().to_string(),
+            quote! { Vec<u16, 4> }.to_string()
+        );
+
+        config.custom_field = Some(CustomField::Delegate("name".to_owned()));
+        let crate::generator::CustomField::Delegate(del) = config.custom_field_parsed().unwrap()
+        else {
+            unreachable!()
+        };
+        assert_eq!(del, format_ident!("name"));
+    }
 }
