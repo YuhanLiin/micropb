616ee4ce6847b6349d150c113a2bcad83413fd2e
YuhanLiin <yuhanliin+github@protonmail.com>
2024-03-10 15:02:18 -0400

    Add field_num to FieldEncode methods

--- micropb/src/field.rs
+++ micropb/src/field.rs
@@ -15,8 +15,12 @@ pub trait FieldDecode {
 
 #[cfg(feature = "encode")]
 pub trait FieldEncode {
-    fn encode_field<W: PbWrite>(&self, encoder: &mut PbEncoder<W>) -> Result<(), W::Error>;
+    fn encode_field<W: PbWrite>(
+        &self,
+        field_num: u32,
+        encoder: &mut PbEncoder<W>,
+    ) -> Result<(), W::Error>;
 
     #[cfg(feature = "encode")]
-    fn compute_field_size(&self) -> usize;
+    fn compute_field_size(&self, field_num: u32) -> usize;
 }
