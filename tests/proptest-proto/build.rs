53f4b47bfd24ef647fa6eb359a96a73df09e1097
YuhanLiin <yuhanliin+github@protonmail.com>
2025-05-24 11:17:16 -0400

    Add optional fields to proptest

--- tests/proptest-proto/build.rs
+++ tests/proptest-proto/build.rs
@@ -3,10 +3,14 @@ use micropb_gen::{Config, Generator};
 fn main() {
     let mut generator = Generator::new();
     // Can only derive Arbitrary on standard containers, so use containers from alloc
-    generator.use_container_alloc().configure(
-        ".",
-        Config::new().type_attributes("#[derive(proptest_derive::Arbitrary)]"),
-    );
+    generator
+        .use_container_alloc()
+        .configure(
+            ".",
+            Config::new().type_attributes("#[derive(proptest_derive::Arbitrary)]"),
+        )
+        // Boxed optional fields will use `Option` instead of hazzers
+        .configure(".TestTypesOptional2", Config::new().boxed(true));
 
     generator
         .compile_protos(
