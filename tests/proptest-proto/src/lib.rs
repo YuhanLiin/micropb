69c648144e6d39d7e50a5b88a2d9b4aad90b2918
YuhanLiin <yuhanliin+github@protonmail.com>
2025-11-09 22:53:43 -0500

    Get prost roundtrip proptest working

--- tests/proptest-proto/src/lib.rs
+++ tests/proptest-proto/src/lib.rs
@@ -7,13 +7,13 @@ mod tests {
     mod micropb_types {
         #![allow(clippy::all)]
         #![allow(nonstandard_style, unused, irrefutable_let_patterns)]
-        include!(concat!(env!("OUT_DIR"), "/all_types.rs"));
+        include!(concat!(env!("OUT_DIR"), "/micropb_all_types.rs"));
     }
 
-    mod proto_types {
+    mod prost_types {
         #![allow(clippy::all)]
         #![allow(nonstandard_style, unused, irrefutable_let_patterns)]
-        include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+        include!(concat!(env!("OUT_DIR"), "/_.rs"));
     }
 
     fn bytes() -> impl proptest::strategy::Strategy<Value = Vec<u8>> {
@@ -37,18 +37,13 @@ mod tests {
     }
 
     fn test_proto_roundtrip(msg: micropb_types::TestOneOf) {
-        use protobuf::Message;
+        use prost::Message;
 
         let mut encoder = PbEncoder::new(vec![]);
         msg.encode(&mut encoder).unwrap();
 
-        println!("{:0x?}", encoder.as_writer().as_slice());
-        let proto_msg =
-            proto_types::all_types::TestOneOf::parse_from_bytes(encoder.as_writer().as_slice())
-                .unwrap();
-        dbg!(&proto_msg);
-        let buf = proto_msg.write_to_bytes().unwrap();
-        println!("{:0x?}", buf);
+        let proto_msg = prost_types::TestOneOf::decode(encoder.as_writer().as_slice()).unwrap();
+        let buf = proto_msg.encode_to_vec();
 
         let mut decoder = PbDecoder::new(buf.as_slice());
         decoder.ignore_wrong_len = true;
@@ -83,42 +78,11 @@ mod tests {
         }
 
         #[test]
-        fn max_size(sin1: proto::TestTypesSingular1, opt1: proto::TestTypesOptional1) {
-            const SIN1_MAX: usize = proto::TestTypesSingular1::MAX_SIZE.unwrap();
+        fn max_size(sin1: micropb_types::TestTypesSingular1, opt1: micropb_types::TestTypesOptional1) {
+            const SIN1_MAX: usize = micropb_types::TestTypesSingular1::MAX_SIZE.unwrap();
             assert!(sin1.compute_size() <= SIN1_MAX);
-            const OPT1_MAX: usize = proto::TestTypesOptional1::MAX_SIZE.unwrap();
+            const OPT1_MAX: usize = micropb_types::TestTypesOptional1::MAX_SIZE.unwrap();
             assert!(opt1.compute_size() <= OPT1_MAX);
         }
-
-        // Decode random data to ensure it doesn't crash
-        #[test]
-        fn decode_random(data in bytes()) {
-            let mut decoder = PbDecoder::new(data.as_slice());
-            let mut msg = proto::TestOneOf::default();
-            if msg.decode(&mut decoder, data.len()).is_ok() {
-                let mut encoder = PbEncoder::new(vec![]);
-                let _ = msg.encode(&mut encoder);
-            }
-        }
-    }
-
-    #[test]
-    fn negative_enum() {
-        use micropb_types::*;
-
-        let msg = TestOneOf {
-            inner: Some(TestOneOf_::Inner::Repeat2(TestTypesRepeated2 {
-                fixed32_field: vec![],
-                fixed64_field: vec![],
-                sfixed32_field: vec![],
-                sfixed64_field: vec![],
-                bool_field: vec![],
-                string_field: vec![],
-                bytes_field: vec![],
-                enum_field: vec![TestEnum(-1), TestEnum(0)],
-                message_field: vec![],
-            })),
-        };
-        test_proto_roundtrip(msg);
     }
 }
