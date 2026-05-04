68a24520c248ab692668f0403f2010c08d2518b7
yuhan <yuhanliin@protonmail.com>
2026-05-03 22:01:59 -0400

    Revert "Separate default_owned and default_ref generators"
    This reverts commit 4b0fe229e40f6c2b6d367180a165062143c35fa8.

--- micropb-gen/src/generator/type_spec.rs
+++ micropb-gen/src/generator/type_spec.rs
@@ -324,19 +324,16 @@ impl<'proto> TypeSpec<'proto> {
             })
     }
 
-    /// Returns default expression and the length of the default string/bytes
-    pub(crate) fn generate_default_ref(
-        &self,
-        default: &str,
-        ctx: &Context<'proto>,
-    ) -> (TokenStream, usize) {
+    pub(crate) fn generate_default(&self, default: &str, ctx: &Context<'proto>) -> TokenStream {
         match self {
-            TypeSpec::String { .. } => (quote! { #default }, default.len()),
+            TypeSpec::String { .. } => {
+                quote! { ::core::convert::TryFrom::try_from(#default).unwrap_or_default() }
+            }
 
             TypeSpec::Bytes { .. } => {
                 let bytes = unescape_c_escape_string(default);
                 let default_bytes = Literal::byte_string(&bytes);
-                (quote! { #default_bytes.as_slice() }, bytes.len())
+                quote! { ::core::convert::TryFrom::try_from(#default_bytes.as_slice()).unwrap_or_default() }
             }
 
             TypeSpec::Message(..) => {
@@ -347,45 +344,14 @@ impl<'proto> TypeSpec<'proto> {
                 let enum_path = ctx.resolve_type_name(tpath);
                 let enum_name = sanitized_ident(&path_suffix(tpath).to_case(Case::Pascal));
                 let variant = ctx.enum_variant_name(default, &enum_name);
-                (quote! { #enum_path::#variant }, 0)
+                quote! { #enum_path::#variant }
             }
 
             _ => {
                 let default: TokenStream =
                     syn::parse_str(default).expect("default value tokenization error");
-                (quote! { #default as _ }, 0)
+                quote! { #default as _ }
             }
-        }
-    }
-
-    pub(crate) fn generate_default_owned(
-        &self,
-        default: &str,
-        ctx: &Context<'proto>,
-    ) -> Result<TokenStream, String> {
-        let (default_ref, default_len) = self.generate_default_ref(default, ctx);
-        match self {
-            TypeSpec::String { max_bytes, .. } => match *max_bytes {
-                Some(max_bytes) if default_len > max_bytes as usize => Err(format!(
-                    "String field is limited to {max_bytes} bytes, but its default value is {} bytes",
-                    default.len()
-                )),
-                _ => Ok(
-                    quote! { ::core::convert::TryFrom::try_from(#default_ref).unwrap_or_default() },
-                ),
-            },
-
-            TypeSpec::Bytes { max_bytes, .. } => match *max_bytes {
-                Some(max_bytes) if default_len > max_bytes as usize => Err(format!(
-                    "Bytes field is limited to {max_bytes} bytes, but its default value is {} bytes",
-                    default.len()
-                )),
-                _ => Ok(
-                    quote! { ::core::convert::TryFrom::try_from(#default_ref).unwrap_or_default() },
-                ),
-            },
-
-            _ => Ok(default_ref),
         }
     }
 
@@ -427,7 +393,7 @@ impl<'proto> TypeSpec<'proto> {
             | TypeSpec::Bool
             | TypeSpec::Enum(_) => {
                 let default_tokens = if let Some(default) = default {
-                    self.generate_default_ref(default, ctx).0
+                    self.generate_default(default, ctx)
                 } else {
                     quote! { ::core::default::Default::default() }
                 };
@@ -787,35 +753,35 @@ mod tests {
         let ctx = make_ctx();
         assert_eq!(
             TypeSpec::Bool
-                .generate_default_owned("true", &ctx)
+                .generate_default("true", &ctx)
                 .unwrap()
                 .to_string(),
             quote! { true as _ }.to_string()
         );
         assert_eq!(
             TypeSpec::Bool
-                .generate_default_owned("false", &ctx)
+                .generate_default("false", &ctx)
                 .unwrap()
                 .to_string(),
             quote! { false as _ }.to_string()
         );
         assert_eq!(
             TypeSpec::Float
-                .generate_default_owned("0.1", &ctx)
+                .generate_default("0.1", &ctx)
                 .unwrap()
                 .to_string(),
             quote! { 0.1 as _ }.to_string()
         );
         assert_eq!(
             TypeSpec::Double
-                .generate_default_owned("-4.1", &ctx)
+                .generate_default("-4.1", &ctx)
                 .unwrap()
                 .to_string(),
             quote! { -4.1 as _ }.to_string()
         );
         assert_eq!(
             TypeSpec::Int(PbInt::Int32, IntSize::S8)
-                .generate_default_owned("-99", &ctx)
+                .generate_default("-99", &ctx)
                 .unwrap()
                 .to_string(),
             quote! { -99 as _ }.to_string()
@@ -825,7 +791,7 @@ mod tests {
                 typestr: "Vec".to_owned(),
                 max_bytes: None
             }
-            .generate_default_owned("abc\n\tddd", &ctx)
+            .generate_default("abc\n\tddd", &ctx)
             .unwrap()
             .to_string(),
             quote! { ::core::convert::TryFrom::try_from("abc\n\tddd").unwrap_or_default() }
@@ -836,7 +802,7 @@ mod tests {
                 typestr: "Vec".to_owned(),
                 max_bytes: None
             }
-            .generate_default_owned("abc\\n\\t\\a\\xA0ddd", &ctx)
+            .generate_default("abc\\n\\t\\a\\xA0ddd", &ctx)
             .unwrap()
             .to_string(),
             quote! { ::core::convert::TryFrom::try_from(b"abc\n\t\x07\xA0ddd".as_slice()).unwrap_or_default() }
